7116efe458fe015e136de5cfa5eb554451d49e37
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-10-20 21:59:42 +0300

    Изменение утилитного класса на интерфейс.

--- src/main/java/tools/FileTools.java
+++ src/main/java/tools/FileTools.java
@@ -1,20 +1,21 @@
 package tools;
 
 /**
- * утилитный класс для работы с файлами
+ * утилитный интерфейс для работы с файлами
  */
-public class FileTools {
+public interface FileTools {
     /**
      * удаляет расширения файла из пути к файлу
+     *
      * @param filepath - путь к файлу
      * @return - путь к файлу без расширения
      */
-    public static String getBaseName(String filepath) {
+    static String getBaseName(String filepath) {
         int index = filepath.lastIndexOf('.');
-        if(index == -1) {
+        if (index == -1) {
             return filepath;
         } else {
-            return filepath.substring(0,index);
+            return filepath.substring(0, index);
         }
     }
 }
